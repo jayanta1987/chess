1eba0a01a62a9705b6313b4c6d3d5cb1a0aabc19
jayanta1987 <51797031+jayanta1987@users.noreply.github.com>
2021-01-01 22:52:15 +0530

    king move completed

--- src/main/java/com/chess/core/King.java
+++ src/main/java/com/chess/core/King.java
@@ -1,6 +1,9 @@
 package com.chess.core;
 
+import java.util.ArrayList;
 import java.util.List;
+
+import com.chess.web.util.ApplicationUtil;
 
 public class King extends Piece {
 	
@@ -12,8 +15,47 @@ public class King extends Piece {
 
 	@Override
 	public List<Location> findPossibleMoves(Location location, String color, Cell[][] board) {
-		// TODO Auto-generated method stub
-		return null;
+		
+    	List<Location> possibleMoves = new ArrayList<>();
+    	int xNum = location.getxNum();
+		int yNum = location.getyNum();
+		
+		Location location1 = new Location(xNum-1, yNum-1);
+		Location location2 = new Location(xNum-1, yNum);
+		Location location3 = new Location(xNum-1, yNum+1);
+		Location location4 = new Location(xNum, yNum-1);
+		Location location5 = new Location(xNum, yNum+1);
+		Location location6 = new Location(xNum+1, yNum-1);
+		Location location7 = new Location(xNum+1, yNum);
+		Location location8 = new Location(xNum+1, yNum+1);
+		
+		if(ApplicationUtil.validatePossibleMove(location1,  color, board)) {
+			possibleMoves.add(location1);
+		}
+		if(ApplicationUtil.validatePossibleMove(location2,  color, board)) {
+			possibleMoves.add(location2);
+		}
+		if(ApplicationUtil.validatePossibleMove(location3,  color, board)) {
+			possibleMoves.add(location3);
+		}
+		if(ApplicationUtil.validatePossibleMove(location4,  color, board)) {
+			possibleMoves.add(location4);
+		}
+		if(ApplicationUtil.validatePossibleMove(location5,  color, board)) {
+			possibleMoves.add(location5);
+		}
+		if(ApplicationUtil.validatePossibleMove(location6,  color, board)) {
+			possibleMoves.add(location6);
+		}
+		if(ApplicationUtil.validatePossibleMove(location7,  color, board)) {
+			possibleMoves.add(location7);
+		}
+		if(ApplicationUtil.validatePossibleMove(location8,  color, board)) {
+			possibleMoves.add(location8);
+		}
+		
+		return possibleMoves;
+		
 	}
 
    
